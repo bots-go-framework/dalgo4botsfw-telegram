d5254357c965e9fbf216d2eda599d1c71ab2c031
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-05-07 05:13:58 +0100

    feat: initial decoupling - tgChatInstance

---
+++ chat_instance.go
@@ -0,0 +1,50 @@
+package dalgo4botsfwtelegram
+
+import (
+	"context"
+	telegram "github.com/bots-go-framework/bots-fw-telegram"
+	botsfwtgmodels "github.com/bots-go-framework/bots-fw-telegram-models"
+	"github.com/dal-go/dalgo/dal"
+	"github.com/dal-go/dalgo/record"
+)
+
+var _ telegram.TgChatInstanceDal = (*tgChatInstanceDalgo)(nil)
+
+type tgChatInstanceDalgo struct {
+	db dal.Database
+}
+
+func (tgChatInstanceDal tgChatInstanceDalgo) GetTelegramChatInstanceByID(c context.Context, tx dal.ReadTransaction, id string) (tgChatInstance botsfwtgmodels.ChatInstance, err error) {
+	tgChatInstance = tgChatInstanceDal.NewTelegramChatInstance(id, 0, "")
+
+	var session dal.ReadSession
+	if tx == nil {
+		session = tgChatInstanceDal.db
+	} else {
+		session = tx
+	}
+	if err = session.Get(c, tgChatInstance.Record); dal.IsNotFound(err) {
+		tgChatInstance.SetEntity(nil)
+		return
+	}
+	return
+}
+
+func (tgChatInstanceDal tgChatInstanceDalgo) SaveTelegramChatInstance(c context.Context, tgChatInstance botsfwtgmodels.ChatInstance) (err error) {
+	err = tgChatInstanceDal.db.RunReadwriteTransaction(c, func(ctx context.Context, tx dal.ReadwriteTransaction) error {
+		return tx.Set(ctx, tgChatInstance.Record)
+	})
+	return
+}
+
+func (tgChatInstanceDalgo) NewTelegramChatInstance(chatInstanceID string, chatID int64, preferredLanguage string) (tgChatInstance botsfwtgmodels.ChatInstance) {
+	key := dal.NewKeyWithID(botsfwtgmodels.ChatInstanceKind, chatInstanceID)
+	var chatInstance botsfwtgmodels.ChatInstanceEntity = &botsfwtgmodels.ChatInstanceEntityBase{
+		TgChatID:          chatID,
+		PreferredLanguage: preferredLanguage,
+	}
+	return botsfwtgmodels.ChatInstance{
+		WithID: record.NewWithID(chatInstanceID, key, chatInstance),
+		Data:   chatInstance,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/bots-go-framework/dalgo4botsfw-telegram
+
+go 1.20
+
+//replace github.com/bots-go-framework/bots-fw-telegram-models => ../bots-fw-telegram-models
+
+//replace github.com/bots-go-framework/bots-fw-telegram => ../bots-fw-telegram
+
+require (
+	github.com/bots-go-framework/bots-fw-telegram v0.4.15
+	github.com/bots-go-framework/bots-fw-telegram-models v0.0.1
+	github.com/dal-go/dalgo v0.2.26
+)
+
+require (
+	github.com/bots-go-framework/bots-api-telegram v0.2.2 // indirect
+	github.com/bots-go-framework/bots-fw v0.14.0 // indirect
+	github.com/bots-go-framework/bots-go-core v0.0.1 // indirect
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7 // indirect
+	github.com/strongo/app v0.3.1 // indirect
+	github.com/strongo/gamp v0.0.1 // indirect
+	github.com/strongo/i18n v0.0.4 // indirect
+	github.com/strongo/log v0.2.3 // indirect
+	github.com/strongo/random v0.0.1 // indirect
+	github.com/strongo/slice v0.1.3 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
